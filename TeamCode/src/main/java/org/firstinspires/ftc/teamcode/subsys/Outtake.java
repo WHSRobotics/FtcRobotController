d3c0a7e8478ccbf419d60f571c59dce935657a6b
Aditya Mukerjee <adityaalexmukerjee@gmail.com>
2021-02-26 16:39:59 -0800

    Worked on outtake

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsys/Outtake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsys/Outtake.java
@@ -1,4 +1,30 @@
 package org.firstinspires.ftc.teamcode.subsys;
 
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
 public class Outtake {
+    DcMotorEx leftSlides;
+    DcMotorEx rightSlides;
+    Servo elbow;
+    Servo wrist;
+
+    public Outtake(HardwareMap outtakeMap){
+        leftSlides = outtakeMap.get(DcMotorEx.class, "leftSlides");
+        rightSlides = outtakeMap.get(DcMotorEx.class, "rightSlides");
+        elbow = outtakeMap.servo.get("elbow");
+        wrist = outtakeMap.servo.get("wrist");
+    }
+
+    public enum Slide_Positions{
+        ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN
+    }
+
+    public enum Elbow_Positions{
+        READY, HOLDING, RELEASING
+    }
+    public enum Wrist_Positions{
+        OPEN, CLOSED
+    }
 }
