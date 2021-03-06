c647f65fb8a14d7bab1b8f033bd06eac483ce753
Aditya Mukerjee <adityaalexmukerjee@gmail.com>
2021-03-05 17:41:36 -0800

    worked on outtake

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsys/Outtake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsys/Outtake.java
@@ -3,17 +3,25 @@ package org.firstinspires.ftc.teamcode.subsys;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.TouchSensor;
+
+import org.firstinspires.ftc.teamcode.lib.util.Toggler;
 
 public class Outtake {
     DcMotorEx leftSlides;
     DcMotorEx rightSlides;
-    Servo elbow;
+    Servo leftElbow;
+    Servo rightElbow;
     Servo wrist;
+
+    Toggler outtakeTog = new Toggler(5);
+    Toggler slideTog = new Toggler(7);
 
     public Outtake(HardwareMap outtakeMap){
         leftSlides = outtakeMap.get(DcMotorEx.class, "leftSlides");
         rightSlides = outtakeMap.get(DcMotorEx.class, "rightSlides");
-        elbow = outtakeMap.servo.get("elbow");
+        leftElbow = outtakeMap.servo.get("leftElbow");
+        rightElbow = outtakeMap.servo.get("rightElbow");
         wrist = outtakeMap.servo.get("wrist");
     }
 
@@ -22,9 +30,35 @@ public class Outtake {
     }
 
     public enum Elbow_Positions{
-        READY, HOLDING, RELEASING
+     HOLDING, RELEASING
     }
     public enum Wrist_Positions{
         OPEN, CLOSED
+    }
+    public int[] SLIDE_POSITIONS = {100, 200, 300, 400, 500, 600, 700};
+    public double[] ELBOW_POSITIONS = {0.1, 0.9};
+    public double[] WRIST_POSITIONS = {0.1, 0.9};
+
+    public void setSlidePositions(Slide_Positions slidePosition){
+        leftSlides.setTargetPosition(SLIDE_POSITIONS[slidePosition.ordinal()]);
+        rightSlides.setTargetPosition(-(SLIDE_POSITIONS[slidePosition.ordinal()]));
+    }
+
+    public void setElbowPositions(Elbow_Positions elbow_position){
+        leftElbow.setPosition(ELBOW_POSITIONS[elbow_position.ordinal()]);
+        rightElbow.setPosition((ELBOW_POSITIONS[elbow_position.ordinal()])-0.5);
+    }
+
+    public void setWristPositions(Wrist_Positions wristPosition){
+        wrist.setPosition(WRIST_POSITIONS[wristPosition.ordinal()]);
+    }
+    public void setOuttakeState(boolean outtakeInputInc, boolean outtakeInputDec, boolean slideInput){
+        outtakeTog.changeState(outtakeInputInc, outtakeInputDec);
+        switch(outtakeTog.currentState()){
+            case 0:
+                setElbowPositions(Elbow_Positions.HOLDING);
+                setWristPositions(Wrist_Positions.OPEN);
+
+        }
     }
 }
