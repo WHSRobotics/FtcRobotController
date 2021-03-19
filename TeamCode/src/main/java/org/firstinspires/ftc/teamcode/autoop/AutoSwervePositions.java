02d752ed620ff4c9cb390af16dba3dc83a3ffef9
Aditya Mukerjee <adityaalexmukerjee@gmail.com>
2021-03-19 16:33:47 -0700

    work

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autoop/AutoSwervePositions.java
@@ -0,0 +1,4 @@
+package org.firstinspires.ftc.teamcode.autoop;
+
+public class AutoSwervePositions {
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsys/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsys/Robot.java
@@ -1,5 +1,4 @@
 package org.firstinspires.ftc.teamcode.subsys;
 
-public class WHSRobotImpl {
-
+public class Robot {
 }
