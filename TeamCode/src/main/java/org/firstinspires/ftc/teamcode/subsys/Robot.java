ce1dcf39d3819c987623045d46764909d2148878
Aditya Mukerjee <adityaalexmukerjee@gmail.com>
2021-03-19 16:34:02 -0700

    work pt2

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsys/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsys/Robot.java
@@ -1,4 +1,21 @@
 package org.firstinspires.ftc.teamcode.subsys;
 
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
 public class Robot {
+    public Intake intake;
+    public Outtake outtake;
+    public Capstone capstone;
+    public Drivetrain drivetrain;
+    public FoundationPullers foundationPullers;
+    public IMU imu;
+
+    public Robot(HardwareMap robotMap){
+        intake = new Intake(robotMap);
+        outtake = new Outtake(robotMap);
+        capstone = new Capstone(robotMap);
+        drivetrain = new Drivetrain(robotMap);
+        foundationPullers = new FoundationPullers(robotMap);
+        imu = new IMU(robotMap);
+    }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TeleOp.java
@@ -5,42 +5,40 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import org.firstinspires.ftc.teamcode.subsys.Capstone;
 import org.firstinspires.ftc.teamcode.subsys.FoundationPullers;
 import org.firstinspires.ftc.teamcode.subsys.Outtake;
+import org.firstinspires.ftc.teamcode.subsys.Robot;
 
 public class TeleOp extends OpMode {
-    FoundationPullers foundationPullers;
-    Capstone capstone;
-    Outtake outtake;
+    Robot robot;
 
     @Override
     public void init() {
-        foundationPullers = new FoundationPullers(hardwareMap);
-        capstone = new Capstone(hardwareMap);
+        robot = new Robot(hardwareMap);
     }
 
     @Override
     public void loop() {
         if (gamepad1.dpad_down || gamepad1.b ){
-
+            robot.intake.turnOnIntake(false);
         }
         // Foundation Pullers
         if (gamepad1.x) {
-            foundationPullers.toggleFoundationPullerPos(gamepad1.x);
+            robot.foundationPullers.toggleFoundationPullerPos(gamepad1.x);
         }
         // Capstone
         if (gamepad1.y) {
-            capstone.setCapstoneState(gamepad1.y);
+            robot.capstone.setCapstoneState(gamepad1.y);
         }
         // Outtake
         if (gamepad1.dpad_left || gamepad1.dpad_right || gamepad1.dpad_up) {
-            outtake.setOuttakeState(gamepad1.dpad_left, gamepad1.dpad_right, gamepad2.dpad_up);
+            robot.outtake.setOuttakeState(gamepad1.dpad_left, gamepad1.dpad_right, gamepad2.dpad_up);
         }
 
 
         // Telemetry
-        telemetry.addData("Foundation Puller Status", foundationPullers.foundationPullerStatus);
-        telemetry.addData("Capstone Status", capstone.capstoneStatus);
-        telemetry.addData("Outtake Elbow Status", outtake.elbowStatus);
-        telemetry.addData("Outtake Wrist Status", outtake.wristStatus);
-        telemetry.addData("Outtake Slides Height", outtake.slidesHeight);
+        telemetry.addData("Foundation Puller Status", robot.foundationPullers.foundationPullerStatus);
+        telemetry.addData("Capstone Status", robot.capstone.capstoneStatus);
+        telemetry.addData("Outtake Elbow Status", robot.outtake.elbowStatus);
+        telemetry.addData("Outtake Wrist Status", robot.outtake.wristStatus);
+        telemetry.addData("Outtake Slides Height", robot.outtake.slidesHeight);
     }
 }
